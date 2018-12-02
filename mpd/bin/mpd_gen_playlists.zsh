22252ac22d9a1ac79b9c695f6e98b16a47ce148c
Sam B <nhalbleiter@gmail.com>
2018-12-02 15:55:39 +0100

    Update README, Add mpd/bin/mpd_gen_playlists.zsh

---
+++ mpd/bin/mpd_gen_playlists.zsh
@@ -0,0 +1,21 @@
+#!/usr/bin/zsh
+zmodload zsh/mapfile
+pathprefix="$(gawk "/^music_directory/ {gsub(\"~\", \"$HOME\"); print substr(\$2, 2, length(\$2) - 2);}" ~/.mpdconf)"
+cd $pathprefix
+excludes=(${(f)mapfile[mpd_exclude.txt]})
+pls=(${(f)"$(mpc lsplaylists)"})
+
+for fn in *; do
+    # Skip files and undesired folders
+    if [[ -f "$fn" ]] || [[ ${excludes[(i)$fn]} -le ${#excludes} ]] {
+        continue
+    }
+    # Skip already existing playlists
+    if [[ ${pls[(i)l$fn]} -le ${#pls} ]] {
+        continue
+    }
+
+    mpc clear
+    mpc ls "$fn" | mpc add
+    mpc save "l$fn"
+done
