0422a5625be4a391250b57e0820f71a7bd827057
Sam B <nhalbleiter@gmail.com>
2018-12-25 00:22:28 +0100

    Rework xmobar temperature script

--- xmonad/bin/getcoretemp.sh
+++ xmonad/bin/getcoretemp.sh
@@ -1,32 +1,30 @@
 #!/bin/bash
-
-function coretemp() {
-    temp=$(sensors | gawk "/Core $1/{\$3=substr(\$3, 2, 2); print \$3}")
-    echo $(formattemp $temp)
+fmt='function formattemp(c) {
+  if (70 < c) {
+    col = "#dc322f"
+  } else if (50 < c) {
+    col = "#859900"
+  } else {
+    col = "#268bd2"
+  }
+  return "<fc=" col ">" c "</fc>"
 }
 
-function gputemp() {
-    temp=$(nvidia-smi --query-gpu=temperature.gpu --format=csv | tail -n1)
-    echo $(formattemp $temp)
+BEGIN { tempstr = "" }
+/^Core/ {
+  temp = substr($3, 2, index($3, ".") - 2)
+  if ($2 == "0:") {
+    tempstr = tempstr formattemp(temp)
+  } else {
+    tempstr = tempstr "|" formattemp(temp)
+  }
 }
+END { print tempstr }'
 
-function formattemp() {
-    if [[ "$1" -gt "70" ]]; then
-	col="#dc322f"
-    elif [[ "$1" -gt "50" ]]; then
-	col="#859900"
-    else
-	col="#268bd2"
-    fi
-    echo "<fc=$col>$1</fc>"
-}
+#function gputemp() {
+#    temp=$(nvidia-smi --query-gpu=temperature.gpu --format=csv | tail -n1)
+#    echo $(formattemp $temp)
+#}
 
-c0=$(coretemp 0)
-c1=$(coretemp 1)
-#c2=$(coretemp 2)
-#c3=$(coretemp 3)
-
-g=$(gputemp)
-
-echo "${c0}|${c1}C"
-# g°
+echo "$(sensors | awk "$fmt")C"
+# °
