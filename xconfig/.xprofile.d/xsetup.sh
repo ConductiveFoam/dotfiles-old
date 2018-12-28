769171203d2a8f93f6bdfc76e2605c5e314bdcca
Sam B <nhalbleiter@gmail.com>
2018-12-28 20:39:47 +0100

    Adjust to new monitor setup

--- xconfig/.xprofile.d/xsetup.sh
+++ xconfig/.xprofile.d/xsetup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 mon_primary='HDMI-1'
 mon_secondary='DVI-D-1'
-xrandr --output "$mon_primary" --primary --output "$mon_secondary" --right-of "$mon_primary"
+xrandr --output "$mon_primary" --primary --output "$mon_secondary" --left-of "$mon_primary" --rotate left
 
 setxkbmap -layout dvorak -option ctrl:nocaps -option compose:ralt
 xset b off
