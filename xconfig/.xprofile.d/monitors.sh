729d698970766eed1816af414595dde09c48a8f7
Sam B <nhalbleiter@gmail.com>
2019-01-01 14:15:57 +0100

    Adjust  to nvidia monitor numbering

--- xconfig/.xprofile.d/monitors.sh
+++ xconfig/.xprofile.d/monitors.sh
@@ -1,3 +1,3 @@
-mon_primary='HDMI-1'
-mon_secondary='DVI-D-1'
+mon_primary='HDMI-0'
+mon_secondary='DVI-D-0'
 xrandr --output "$mon_primary" --primary --output "$mon_secondary" --left-of "$mon_primary" --rotate left
