386d95fc88e3b167ca8634125ff79dc81b0b6f38
Sam B <nhalbleiter@gmail.com>
2019-03-23 13:02:17 +0100

    Remap a few keys for convenience

--- xconfig/.xprofile.d/xsetup.sh
+++ xconfig/.xprofile.d/xsetup.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/sh
 setxkbmap -layout dvorak -option ctrl:nocaps -option compose:ralt
+# Map XF86Back and XF86Forward from (browser) navigation
+xmodmap -e "keycode 166 = Prior" -e "keycode 167 = Next"
+# Unmap XF86Mail and XF86Favorites
+xmodmap -e "keycode 163 = NoSymbol" -e "keycode 164 = NoSymbol"
 xset b off
 numlockx
 
