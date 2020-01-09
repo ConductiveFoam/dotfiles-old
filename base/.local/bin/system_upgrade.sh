e4b0f39c012f4564b259f4675f8c848c7c89034e
Sam B <nhalbleiter@gmail.com>
2020-01-10 00:35:06 +0100

    Improve system_upgrade.sh
    - Only use actual ssh key fingerprint for comparison with the loaded keys,
      not the whole public key file's content

--- base/.local/bin/system_upgrade.sh
+++ base/.local/bin/system_upgrade.sh
@@ -9,7 +9,7 @@ tput sgr0
 # Add keys that aren't already in the agent
 #  This assumes the corresponding private files are without suffix ".pub"
 for fn in $identity_files; do
-    [[ -n $(ssh-add -L | grep -F "$(cat $fn)") ]] || ssh-add "${fn%.*}"
+    [[ -n $(ssh-add -L | grep -F "$(cat $fn | cut -d ' ' -f2)") ]] || ssh-add "${fn%.*}"
 done
 
 topgrade
