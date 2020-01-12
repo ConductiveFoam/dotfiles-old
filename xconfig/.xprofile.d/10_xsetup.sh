db6b47acd33a5e593bfa70bbaebf21450ec81c8f
Sam B <nhalbleiter@gmail.com>
2020-01-12 01:48:06 +0100

    Add IMWheel+config, Document x startup script

--- xconfig/.xprofile.d/10_xsetup.sh
+++ xconfig/.xprofile.d/10_xsetup.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/sh
+# Keyboard setup: Dvorak keymap, custom binds, enable numlock
 setkeyboard.sh dvorak
-xset b off
 numlockx
 
-systemctl --user start udiskied
+# MX Ergo config
+imwheel
 
+# Disable beeps
+xset b off
+
+# Screensaver: Startup, deactivation timer
 xscreensaver -no-splash &
 systemctl --user start xss-deactivate.timer
+
+systemctl --user start udiskied
