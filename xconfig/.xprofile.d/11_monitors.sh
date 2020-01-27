6384fba57cadf82bc84cfe75be308b1e11712d9c
Sam B <nhalbleiter@gmail.com>
2020-01-27 19:10:17 +0100

    Change bus of secondary monitor

--- xconfig/.xprofile.d/11_monitors.sh
+++ xconfig/.xprofile.d/11_monitors.sh
@@ -1,6 +1,6 @@
 if [[ $(xrandr | grep " connected" | wc -l) -eq 2 ]]; then
     mon_primary='HDMI-0'
-    mon_secondary='DVI-D-0'
+    mon_secondary='DVI-I-1'
     xrandr --output "$mon_primary" --primary --output "$mon_secondary" --left-of "$mon_primary" --rotate left
 else
     xrandr --output "$(xrandr | grep " connected" | cut -d' ' -f1)" --primary
