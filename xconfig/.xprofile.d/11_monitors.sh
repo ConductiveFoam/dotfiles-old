3a25df303e81556db6a3832de70e2411fc6473b7
Sam B <nhalbleiter@gmail.com>
2021-05-06 22:12:45 +0200

    Update config for new monitors
    - Set DPI via xrdb
    - Adjust redshift config

--- xconfig/.xprofile.d/11_monitors.sh
+++ xconfig/.xprofile.d/11_monitors.sh
@@ -1,7 +1,9 @@
 if [[ $(xrandr | grep " connected" | wc -l) -eq 2 ]]; then
-    mon_primary='HDMI-0'
-    mon_secondary='DVI-I-1'
+    mon_primary='DisplayPort-0'
+    mon_secondary='DisplayPort-1'
     xrandr --output "$mon_primary" --primary --output "$mon_secondary" --left-of "$mon_primary" --rotate left
 else
     xrandr --output "$(xrandr | grep " connected" | cut -d' ' -f1)" --primary
 fi
+
+[ -f ~/.fehbg ] && ~/.fehbg
